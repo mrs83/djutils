f18fdd21ef867fd4c6c320da0dd611d0d0c9b482
massimo.scamarcia <massimo.scamarcia@3a387afe-f350-0410-8621-5dd5e4302102>
2009-05-13 15:44:30 +0000

    get_user_comments template tag

--- trunk/djutils/templatetags/djutils/comments.py
+++ trunk/djutils/templatetags/djutils/comments.py
@@ -1,5 +1,7 @@
 from django import template
+from django.db.models import get_model
 from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
@@ -30,3 +32,47 @@ def do_get_comment_count_for_user(parser, token):
     return UserCommentCountNode(bits[1], bits[3])
 
 register.tag('get_comment_count_for_user', do_get_comment_count_for_user)
+
+class UserCommentsNode(template.Node):
+    def __init__(self, user, context_var, model=None):
+        self.user = user
+        self.context_var = context_var
+        self.model = model
+
+    def render(self, context):
+        user = template.Variable(self.user).resolve(context)
+        queryset = Comment.objects.filter(user=user, is_public=True)
+        if self.model:
+            model = get_model(*self.model.split('.'))
+            ctype = ContentType.objects.get_for_model(model)
+            queryset = queryset.filter(content_type=ctype)
+        context[self.context_var] = queryset
+        return ''
+
+def do_get_comments_for_user(parser, token):
+    """
+    Get user's comment count as context template variable
+
+    Usage::
+        
+        {% get_comments_for_user user as comment_list %}
+        {% get_comments_for_user user "blog.Post" as comment_list %}
+    """
+    bits = token.contents.split()
+    bits_len = len(bits)
+    if bits_len < 4:
+        raise template.TemplateSyntaxError, "tag takes 3 arguments"
+    user = bits[1]
+    if bits_len == 5:
+        model = bits[2]
+        context_var = bits[4]
+        if bits[3] != 'as':
+            raise template.TemplateSyntaxError, "3rd argument must be 'as'"
+    else:
+        model = None
+        context_var = bits[3]
+        if bits[2] != 'as':
+            raise template.TemplateSyntaxError, "2nd argument must be 'as'"
+    return UserCommentsNode(user, context_var, model)
+
+register.tag('get_comments_for_user', do_get_comments_for_user)
