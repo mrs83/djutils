0db6d98d3c7f0f1503d46e2524922f626815e6af
massimo.scamarcia <massimo.scamarcia@3a387afe-f350-0410-8621-5dd5e4302102>
2010-01-25 13:41:57 +0000

    addthis fix

--- djutils/templatetags/djutils_tags/addthis.py
+++ djutils/templatetags/djutils_tags/addthis.py
@@ -1,6 +1,7 @@
 from django import template
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -8,6 +9,8 @@ def add_this(title, url):
     if not hasattr(settings, 'ADD_THIS_USERNAME'):
         raise ImproperlyConfigured, 'You need to set ADD_THIS_USERNAME '\
                                     'into your own settings.py file'
-    return {'url': url, 'title': title, 'username': username}
+    username = settings.ADD_THIS_USERNAME
+    site = Site.objects.get_current()
+    return {'title': title, 'url': url, 'username': username, 'site': site}
 
 register.inclusion_tag('tags/add_this.html')(add_this)
